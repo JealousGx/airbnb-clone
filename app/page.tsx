44485f6b4b889a14363849d844963f6244019746
Matt <59867217+JealousGx@users.noreply.github.com>
2023-05-27 23:30:39 +0500

    dynamic_server_error fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic'
+
 import { getCurrentUser } from "./actions/getCurrentUser";
 import getListings, { IListings } from "./actions/getListings";
 import ClientOnly from "./components/ClientOnly";
